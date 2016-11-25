6dc7287678eefed49c344930f8b9725a22a8360c
nikewu <862751557@qq.com>
2016-11-25 09:55:23 +0800

    add instroduction and fix init

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # core-seed
 一个可以使用scss进行重新定制的简单的纯css实现的一个css基础库
 包含：flex栅栏格,模态框,按钮,浮动输入框,提示文字,复选框,加载框
-首先npm install npm run dev 进行编写  npm run release将所有发布到style.min.css
-
+首先npm install 然后 npm run dev 进行编写  npm run release将所有发布到style.min.css
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var minifycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var  del = require('del');
 var sass = require('gulp-ruby-sass');
-var concat = require('gulp-concat');                 
+var concat = require('gulp-concat');
 gulp.task('watchsass',function () {
     gulp.watch('src/sass/**/*', function (event) {
         var paths = watchPath(event, 'src/sass/', 'dist/')
@@ -19,16 +19,26 @@ gulp.task('watchsass',function () {
             .pipe(gulp.dest(paths.distDir))
     })
 })
-gulp.task('concat', function() {    
-    gulp.src(['./dist/*.css'])  
-        .pipe(concat('style.min.css')) 
+gulp.task('concat', function() {
+    gulp.src(['./dist/*.css'])
+        .pipe(concat('style.min.css'))
         .pipe(sourcemaps.init())
-        .pipe(minifycss()) 
-        .pipe(sourcemaps.write('./'))                                                             
-        .pipe(gulp.dest('./dist/main/'))                            
+        .pipe(minifycss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/main/'))
 });
+gulp.task('sasscss', function () {
+        sass('src/sass/*.scss')
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(autoprefixer({
+          browsers: 'last 2 versions'
+        }))
+        .pipe(gulp.dest('dist/'))
+})
 gulp.task('default', [
-   'watchsass'
+    'sasscss','watchsass'
     ]
 )
 gulp.task('release', ['concat']);
